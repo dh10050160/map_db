ea221a40e895f48cafef093d2362a4063222c54b
Annie <dh10050160@gmail.com>
2024-03-06 14:30:19 +0800

    port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg'); // import pg module
 const app = express(); // initialize express
 
 // const port = process.env.PORT || 5000; //heroku
-const port = 3000; //8080
+const port = 80;
 // const host = 'localhost';
 // 配置 PostgreSQL local-local
 // const pool = new Pool({
